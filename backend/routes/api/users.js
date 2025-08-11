7e90437caf54b4c6ac35a3d79529bef08d45a609
Jaz <62318169+jasrayatwal@users.noreply.github.com>
2025-08-10 20:30:06 -0700

    add user input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,14 +1,45 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1 })
+    .withMessage('Please provide a first name with at least 1 character.'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1 })
+    .withMessage('Please provide a last name with at least 1 character.'),
+  check('firstName')
+    .not()
+    .isEmail()
+    .withMessage('First name cannot be an email.'),
+  check('lastName')
+    .not()
+    .isEmail()
+    .withMessage('Last name cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
 // Sign up
 router.post(
   '/',
+  validateSignup,
   async (req, res) => {
     const { email, password, firstName, lastName } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
