197ce909c256ba1baca6a6d4dd7d439d320527c2
Jaz <62318169+jasrayatwal@users.noreply.github.com>
2025-08-11 02:49:14 -0700

    fix CSRF to load in development, remove test api/test route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,8 +8,4 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
-
 module.exports = router;
